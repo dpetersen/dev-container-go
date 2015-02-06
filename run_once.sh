9dec3645f227d085c2cb1e383fdd73cf6d5f9f92
Don Petersen <don@donpetersen.net>
2015-02-06 20:28:20 +0000

    Add a boatload of additional tools to run_once.

--- run_once.sh
+++ run_once.sh
@@ -3,7 +3,18 @@
 mkdir -p /root/gopath/src /root/gopath/bin /root/gopath/pkg  
 
 echo "Installing tools..."
-go get golang.org/x/tools/cmd/...
+go get -v golang.org/x/tools/cmd/...
+
+# The following were chosen because they're what vim-go installs if you
+# ask it to. They're all pretty helpful to have around.
+go get -v github.com/nsf/gocode
+go get -v golang.org/x/tools/cmd/goimports
+go get -v code.google.com/p/rog-go/exp/cmd/godef
+go get -v golang.org/x/tools/cmd/oracle
+go get -v golang.org/x/tools/cmd/gorename
+go get -v github.com/golang/lint/golint
+go get -v github.com/kisielk/errcheck
+go get -v github.com/jstemmer/gotags
 
 echo "Installing godep..."
-go get github.com/tools/godep
+go get -v github.com/tools/godep
