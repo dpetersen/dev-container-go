21d081fb45bd13804772564d0bf544abef427f08
Don Petersen <don@donpetersen.net>
2015-11-30 14:07:37 -0800

    Add impl to run-once.sh.

--- run_once.sh
+++ run_once.sh
@@ -5,6 +5,9 @@ mkdir -p /root/gopath/src /root/gopath/bin /root/gopath/pkg
 echo "Installing tools..."
 go get -v golang.org/x/tools/cmd/...
 
+echo "Installing impl..."
+go get -v github.com/josharian/impl
+
 # These are helpful dependencies to get vim-go's full functionality, and many
 # of them are helpful on their own.
 echo "Running vim-go's GoInstallBinaries..."
