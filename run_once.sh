e187841662261fc23b8515687154c827ea969985
Don Petersen <don@donpetersen.net>
2017-12-08 15:12:21 -0800

    Update run_once.sh script.
    Not sure where I got that `go get -a` thing from, but it hasn't worked for a while.

--- run_once.sh
+++ run_once.sh
@@ -3,19 +3,19 @@
 mkdir -p $GOPATH
 
 echo "Installing tools..."
-go get -v golang.org/x/tools/cmd/...
+go get -v -u golang.org/x/tools/cmd/...
 
 echo "Installing impl..."
-go get -v github.com/josharian/impl
+go get -v -u github.com/josharian/impl
 
 echo "Installing protoc-gen-go..."
-go get -a github.com/golang/protobuf/protoc-gen-go
+go get -v -u github.com/golang/protobuf/protoc-gen-go
 
 echo "Installing pp..."
-go get -v github.com/k0kubun/pp
+go get -v -u github.com/k0kubun/pp
 
 echo "Installing godep..."
-go get -v github.com/tools/godep
+go get -v -u github.com/tools/godep
 
 # These are helpful dependencies to get vim-go's full functionality, and many
 # of them are helpful on their own.
